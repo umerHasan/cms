8d277de99ef390e4d5bd9d21d6c8f240f14fb23b
Umer <mumerhasan@live.co.uk>
2025-08-20 23:09:59 +0500

    installed filament, made sections

---
+++ resources/views/sections/popular-products.blade.php
@@ -0,0 +1,46 @@
+<div class="popular-product">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                <div class="product-item-sm d-flex">
+                    <div class="thumbnail">
+                        <img src="{{ asset('assets/images/product-1.png') }}" alt="Image" class="img-fluid">
+                    </div>
+                    <div class="pt-3">
+                        <h3>Nordic Chair</h3>
+                        <p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
+                        <p><a href="#">Read More</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                <div class="product-item-sm d-flex">
+                    <div class="thumbnail">
+                        <img src="{{ asset('assets/images/product-2.png') }}" alt="Image" class="img-fluid">
+                    </div>
+                    <div class="pt-3">
+                        <h3>Kruzo Aero Chair</h3>
+                        <p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
+                        <p><a href="#">Read More</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                <div class="product-item-sm d-flex">
+                    <div class="thumbnail">
+                        <img src="{{ asset('assets/images/product-3.png') }}" alt="Image" class="img-fluid">
+                    </div>
+                    <div class="pt-3">
+                        <h3>Ergonomic Chair</h3>
+                        <p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
+                        <p><a href="#">Read More</a></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
